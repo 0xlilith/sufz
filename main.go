400ce4bfce18ecb00ce97fcec1704845f06bee9e
0xlilith <0xlilith@protonmail.com>
2022-09-15 22:52:20 +0530

    add: Configeration

--- main.go
+++ main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"sufz/sufz"
-	"flag"
 	"bufio"
+	"flag"
+	"fmt"
 	"os"
 )
 
+type Config struct {
+    Threads int `json:"threads"`
+    Depth   int `json:"depth"`
+    Timeout int `json:"timeout"`
+}
+
+func LoadConfig(filename string) (Config, error) {
+	var config Config
+	configFile, err := os.Open(filename)
+	defer configFile.Close()
+	if err != nil {
+		return config, err
+	}
+	jsonParse := json.NewDecoder(configFile)
+	err = jsonParse.Decode(&config)
+	return config, err
+}
+
 func main()  {
-	// FLAGS libray
+	// Config
+	config, _ := LoadConfig("config.json")
+	fmt.Println(config)
+
+	// FLAGS
 	urlencode := flag.Bool("e", false, "Url Encode")
 	urldecode := flag.Bool("d", false, "Url Decode")
 
